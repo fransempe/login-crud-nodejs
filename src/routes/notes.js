7d2d54cf19a7d9b5b0037c9ae69cc9aa39c27f7d
FRANCISCO\Francisco <fran.sempe@gmail.com>
2019-10-08 16:05:43 -0300

    Crud note

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router= express.Router();
 
+const Note = require('../models/Note');
+
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note')
 });
 
-router.post('/notes/new-note', (req, res) => {
+router.post('/notes/new-note',  async (req, res) => {
     const { title, description } = req.body;
     const errors = [];
     if (!title){
@@ -21,12 +23,31 @@ router.post('/notes/new-note', (req, res) => {
             description
         })
     } else{
-        res.send('ok')
+        const newNote = new Note({ title, description });
+        await newNote.save();
+        res.redirect('/notes')
     }
 });
 
-router.get('/notes', (req, res) => {
-    res.send('Notes from database');
+router.get('/notes', async (req, res) => {
+   const notes = await Note.find().sort({date:'desc'});
+   res.render('notes/all-notes',{ notes });
+
 });
 
+router.get('/notes/edit/:id', async (req, res) =>{
+    const note = await Note.findById(req.params.id)
+    res.render('notes/edit-note', {note});
+});
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description })
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.redirect("/notes")
+})
 module.exports = router;
