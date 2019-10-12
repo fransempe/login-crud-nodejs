889ee92c1b1e8b776646788e598a544143a3c23b
fransempe <fran.sempe@gmail.com>
2019-10-12 12:41:41 -0300

    Ended app

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 
 const app = express();
 require('./database');
+require('./config/passport');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -29,12 +31,15 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Globals Variables
 app.use(( req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
-
+    res.locals.error = req.flash('error')
+    res.locals.user = req.user || null;
     next();
 });
 
